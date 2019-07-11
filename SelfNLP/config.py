3b30ad6c1b92a2714a6ea2a54e54099ff301f736
Noordeen <nursnaaz@gmail.com>
2019-07-11 11:26:17 +0530

    Update config.py

--- SelfNLP/config.py
+++ SelfNLP/config.py
@@ -1,8 +1,8 @@
 import os
 
-S3_BUCKET                 = os.environ.get("corp-datainsight-sagemaker-data")
-S3_KEY                    = os.environ.get("AKIAJQ7EO4RG5SQCVC4Q")
-S3_SECRET                 = os.environ.get("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")
+S3_BUCKET                 = os.environ.get("")
+S3_KEY                    = os.environ.get("")
+S3_SECRET                 = os.environ.get("")
 S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
 SECRET_KEY                = os.urandom(32)
